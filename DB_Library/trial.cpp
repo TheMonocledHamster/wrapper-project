d2872772653ca58c0b038eb51d1cfb9b6430cede
Emperor-Adi <adithyah07@gmail.com>
2021-07-20 13:06:47 +0530

    :-\

--- DB_Library/trial.cpp
+++ DB_Library/trial.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 #include <string>
 #include<fstream>
+#include <vector>
 #include "DBLib.hpp"
 
 using namespace std;
 
 int main()
 {
-    DB data;
-    string key;
-    cout << "Colour name: ";
-    cin >> key;
-    string color = data.retrieve("ANSIcolorcodes.json",key);
-    if (color=="FileNotFound" || color=="ParseError" || color=="KeyNotFound")
-        color = "0";
-    string out = data.retrieve("SixBitColours.json",key);
-    if(out!="FileNotFound"){
-        if(out!="ParseError"){
-            if(out!="KeyNotFound"){
-                cout << "Hex code: " << "\033[" << color << ";5m"<< out << "\033[0m" << endl;
-            }
-            else{
-                cout << "Key does not exist" << endl;
-            }
-        }
-        else{
-            cout << "Couldn't parse datafile, check formatting" << endl;
-        }
-    }
-    else{
-        cout << "File does not exist" << endl;
-    }
-
-    return 0;
+	vector<string> get_colors{};
+	DB data;
+	string key;
+	cout << "Enter 'done' once all the colors are entered" << endl;
+	while(true)
+	{
+		cout << "color name: ";
+		cin >> key;
+		if(key=="done")
+			break;
+		get_colors.push_back(key);
+	}
+	
+	for(auto it = get_colors.begin(); it != get_colors.end(); ++it)
+	{
+	string color = data.retrieve("ANSIcolorcodes.json",*it);
+	if (color=="FileNotFound" || color=="ParseError" || color=="KeyNotFound")
+		color = "0";
+	string out = data.retrieve("SixBitcolors.json",*it);
+	if(out!="FileNotFound"){
+		if(out!="ParseError"){
+			if(out!="KeyNotFound"){
+				cout << "Color: " << *it << endl << "Hex code: " << "\033[" << color << ";5m"<< out << "\033[0m" << endl << endl;
+			}
+			else{
+				cout << "Key does not exist" << endl;
+			}
+		}
+		else{
+			cout << "Couldn't parse datafile, check formatting" << endl;
+		}
+	}
+	else{
+		cout << "File does not exist" << endl;
+	}
+	}
+	return 0;
 }
