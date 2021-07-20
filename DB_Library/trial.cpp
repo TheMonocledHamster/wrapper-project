3ffcdba75ee1cf35b262843eef236d03574097a5
AdityaSubrahmanyaBhat <adityasbhat1234@gmail.com>
2021-07-20 19:47:06 +0530

    template class added

--- DB_Library/trial.cpp
+++ DB_Library/trial.cpp
@@ -1,49 +1,88 @@
 #include <iostream>
 #include <string>
-#include<fstream>
+#include <fstream>
 #include <vector>
 #include "DBLib.hpp"
 
 using namespace std;
 
-int main()
+template <class B>
+class Base
 {
-	vector<string> get_colors{};
+protected:
+	vector<B> get_colors;
 	DB data;
+	public:
+	Base() {}
+};
+
+template <class D>
+class Derived : public Base<D>{
+public:
+	Derived() {}
 	string key;
+	vector<D> get_colors;
+	DB data;
+	void read();
+	void getColor();
+};
+
+template <class D>
+void Derived<D>::read()
+{
 	cout << "Enter 'done' once all the colors are entered" << endl;
-	while(true)
+	int i=0;
+	while (true)
 	{
-		cout << "color name: ";
+		cout << "color "<<i++<<" : ";
 		cin >> key;
-		if(key=="done")
+		if (key == "done")
 			break;
 		get_colors.push_back(key);
 	}
-	
-	for(auto it = get_colors.begin(); it != get_colors.end(); ++it)
+}
+
+template <class D>
+void Derived<D>::getColor()
+{
+	for (auto it = get_colors.begin(); it != get_colors.end(); ++it)
 	{
-	string color = data.retrieve("ANSIcolorcodes.json",*it);
-	if (color=="FileNotFound" || color=="ParseError" || color=="KeyNotFound")
-		color = "0";
-	string out = data.retrieve("SixBitColors.json",*it);
-	if(out!="FileNotFound"){
-		if(out!="ParseError"){
-			if(out!="KeyNotFound"){
-				cout<<endl << "Color: " << *it << endl << "Hex code: "
-                     << "\e[1;" << color << "m" << out << "\e[0m" << endl;
+		string color = data.retrieve("ANSIcolorcodes.json", *it);
+		if (color == "FileNotFound" || color == "ParseError" || color == "KeyNotFound")
+			color = "0";
+		string out = data.retrieve("SixBitColors.json", *it);
+		if (out != "FileNotFound")
+		{
+			if (out != "ParseError")
+			{
+				if (out != "KeyNotFound")
+				{
+					cout << endl
+						 << "Color: " <<"\e[1;" << color << "m" << *it << "\e[0m" << endl
+						 << "Hex code: "
+						 << "\e[1;" << color << "m" << out << "\e[0m" << endl;
+				}
+				else
+				{
+					cout<<endl << "Key does not exist" << endl;
+				}
 			}
-			else{
-				cout << "Key does not exist" << endl;
+			else
+			{
+				cout <<endl<< "Couldn't parse datafile, check formatting" << endl;
 			}
 		}
-		else{
-			cout << "Couldn't parse datafile, check formatting" << endl;
+		else
+		{
+			cout <<endl<< "File does not exist" << endl;
 		}
 	}
-	else{
-		cout << "File does not exist" << endl;
-	}
-	}
+}
+
+int main()
+{
+	Derived<string> derived;
+	derived.read();
+	derived.getColor();
 	return 0;
 }
